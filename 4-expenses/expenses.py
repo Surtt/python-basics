f572580252df24775e2db2a153aaf9894d6f64d9
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2026-01-11 21:35:38 +0200

    feat: add expenses

---
+++ 4-expenses/expenses.py
@@ -0,0 +1,9 @@
+expenses = [20, 35, 15, 40, 10, 28, 50]
+sum_expenses = sum(expenses)
+average_expense = sum_expenses / len(expenses)
+min_expense = min(expenses)
+max_expense = max(expenses)
+print(f"Total Expenses: ${sum_expenses}")
+print(f"Average Expense: ${average_expense:.2f}")
+print(f"Minimum Expense: ${min_expense}")
+print(f"Maximum Expense: ${max_expense}")
