0c3a0b31123dabfc90301794c7e1d18b7746cb8c
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2026-01-13 22:16:03 +0200

    feat: add sum formatter

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,7 +1,24 @@
-expenses = [20, 35, 15, 40, 10, 28, 50]
-sum_expenses = sum(expenses)
-average_expense = sum_expenses / len(expenses)
-min_expense = min(expenses)
-max_expense = max(expenses)
-all_expenses = (min_expense, max_expense, sum_expenses)
-print("All expenses:", all_expenses)
+price = input("Введите цену: ")
+
+formatted_str = price.strip().lower()
+splitted_list = formatted_str.split(" ")
+
+if len(splitted_list) < 2:
+    print("Некорректный формат суммы")
+else:
+    if "руб" in splitted_list[1]:
+        rub = splitted_list[0]
+
+        if not rub.isdigit():
+            print("Некорректный формат суммы")
+        else:
+            if len(splitted_list) >= 4 and "коп" in splitted_list[3]:
+                kop = splitted_list[2]
+
+                if not kop.isdigit():
+                    print("Некорректный формат суммы")
+                else:
+                    kop_formatted = kop.zfill(2)
+                    print(f"{rub}.{kop_formatted} ₽")
+            else:
+                print(f"{rub}.00 ₽")
