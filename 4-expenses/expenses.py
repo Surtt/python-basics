cab243e443c2f9a4746b32242e22ed37389a1659
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2026-01-19 22:05:42 +0200

    feat: add menu

--- 4-expenses/expenses.py
+++ 4-expenses/expenses.py
@@ -1,24 +1,43 @@
-price = input("Введите цену: ")
+# price = input("Введите цену: ")
+#
+# formatted_str = price.strip().lower()
+# splitted_list = formatted_str.split(" ")
+#
+# if len(splitted_list) < 2:
+#     print("Некорректный формат суммы")
+# else:
+#     if "руб" in splitted_list[1]:
+#         rub = splitted_list[0]
+#
+#         if not rub.isdigit():
+#             print("Некорректный формат суммы")
+#         else:
+#             if len(splitted_list) >= 4 and "коп" in splitted_list[3]:
+#                 kop = splitted_list[2]
+#
+#                 if not kop.isdigit():
+#                     print("Некорректный формат суммы")
+#                 else:
+#                     kop_formatted = kop.zfill(2)
+#                     print(f"{rub}.{kop_formatted} ₽")
+#             else:
+#                 print(f"{rub}.00 ₽")
 
-formatted_str = price.strip().lower()
-splitted_list = formatted_str.split(" ")
+MENU_LIST = (
+    "Добавить расход",
+    "Показать все расходы",
+    "Показать сумму и средний расход",
+    "Удалить расход по номеру",
+    "Выход",
+)
 
-if len(splitted_list) < 2:
-    print("Некорректный формат суммы")
-else:
-    if "руб" in splitted_list[1]:
-        rub = splitted_list[0]
-
-        if not rub.isdigit():
-            print("Некорректный формат суммы")
-        else:
-            if len(splitted_list) >= 4 and "коп" in splitted_list[3]:
-                kop = splitted_list[2]
-
-                if not kop.isdigit():
-                    print("Некорректный формат суммы")
-                else:
-                    kop_formatted = kop.zfill(2)
-                    print(f"{rub}.{kop_formatted} ₽")
-            else:
-                print(f"{rub}.00 ₽")
+while True:
+    print()
+    for index, item in enumerate(MENU_LIST, start=1):
+        print(f"{index}. {item}")
+    users_answer = int(input("Выберите пункт меню:\n"))
+    if users_answer == 5:
+        print("До свидания!")
+        break
+    else:
+        print("В разработке...\n")
