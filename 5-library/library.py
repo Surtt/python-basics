3544df684ff9c8c27a25d0e98104f13e4c920124
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2026-02-04 21:57:15 +0200

    feat: add errors

--- 5-library/library.py
+++ 5-library/library.py
@@ -1,5 +1,22 @@
 import sys
 
+
+class LibraryError(Exception):
+    pass
+
+
+class UnknownCommandError(LibraryError):
+    pass
+
+
+class InvalidSortKeyError(LibraryError):
+    pass
+
+
+class EmptyFilterKeyError(LibraryError):
+    pass
+
+
 books = {"Война и мир": "Лев Толстой", "Мастер и Маргарита": "Михаил Булгаков"}
 books["Преступление и наказание"] = "Федор Достоевский"
 books["Анна Каренина"] = "Лев Толстой"
@@ -10,25 +27,38 @@ books["Отцы и дети"] = "Иван Тургенев"
 def sort_books(books_dict, sort_by):
     if sort_by == "author":
         sorted_books = sorted(books_dict.items(), key=lambda item: item[1])
-    else:
+    elif sort_by == "title":
         sorted_books = sorted(books_dict.items(), key=lambda item: item[0])
+    else:
+        raise InvalidSortKeyError(f"Неверный ключ сортировки: {sort_by}")
 
     return list(map(lambda item: f"{item[0]} — {item[1]}", sorted_books))
 
 
 def filter_books(books_dict, author_filter):
+    if not author_filter:
+        raise EmptyFilterKeyError("Ключ фильтрации не может быть пустым")
     filtered = filter(lambda item: author_filter in item[1], books_dict.items())
     return list(map(lambda item: f"{item[0]} — {item[1]}", filtered))
 
 
 match sys.argv[1]:
     case "sort":
-        result = sort_books(books, sys.argv[2])
-        for line in result:
-            print(line)
+        try:
+            result = sort_books(books, sys.argv[2])
+            for line in result:
+                print(line)
+        except InvalidSortKeyError as e:
+            print(e)
     case "filter":
-        result = filter_books(books, sys.argv[2])
-        for line in result:
-            print(line)
+        try:
+            result = filter_books(books, sys.argv[2])
+            for line in result:
+                print(line)
+        except EmptyFilterKeyError as e:
+            print(e)
     case _:
-        print("Неизвестная команда. Используйте: sort или filter")
+        try:
+            raise UnknownCommandError(f"Неизвестная команда: {sys.argv[1]}")
+        except UnknownCommandError as e:
+            print(e)
